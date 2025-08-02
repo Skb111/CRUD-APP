640e90143f391a3f190f06a9e5135578a512dc54
Sadiq Kabir <webemail655@gmail.com>
2025-08-03 00:46:47 +0100

    Full CRUD Operation with React Native

---
+++ CrudApp/data/todos.js
@@ -0,0 +1,27 @@
+export const data = [
+    {
+        "id": 1,
+        "title": "Finish reading the book",
+        "completed": true,
+    },
+    {
+        "id": 2,
+        "title": "Finish the project",
+        "completed": false,
+    },
+    {
+        "id": 3,
+        "title": "Learn React",
+        "completed": true,
+    },
+    {
+        "id": 4,
+        "title": "Go to the gym",
+        "completed": false,
+    },
+    {
+        "id": 5,
+        "title": "Finish the project",
+        "completed": true,
+    }
+]
