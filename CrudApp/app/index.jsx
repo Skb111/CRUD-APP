89b222dc38d4fff3e38d835340ad58bb7267e96f
Sadiq Kabir <webemail655@gmail.com>
2025-08-03 17:20:30 +0100

    Added Status Bar

--- CrudApp/app/index.jsx
+++ CrudApp/app/index.jsx
@@ -1,10 +1,16 @@
 import { Text, View, TextInput, Pressable, StyleSheet, FlatList } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
-import { useState, useContext } from "react";
+import { useState, useContext, useEffect } from "react";
 import { ThemeContext } from '../context/ThemeContext';
 import MaterialCommunityIcons from '@expo/vector-icons/MaterialCommunityIcons';
 
 import { Inter_500Medium, useFonts } from "@expo-google-fonts/inter";
+
+import Animated, { LinearTransition } from 'react-native-reanimated';
+
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+import { StatusBar } from "expo-status-bar";
 
 import Octicons from '@expo/vector-icons/Octicons'
 
@@ -14,17 +20,48 @@ export default function Index() {
   const [todos, setTodos] = useState(data.sort((a, b) => b.id - a.id));
   const [text, setText] = useState('')
 
-  const {colorScheme, setColorScheme,theme} = useContext(ThemeContext)
+  const { colorScheme, setColorScheme, theme } = useContext(ThemeContext)
 
   const [loaded, error] = useFonts({
     Inter_500Medium,
   })
 
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const jsonValue = await AsyncStorage.getItem("TodoApp")
+        const storageTodos = jsonValue != null ? JSON.parse(jsonValue) : null
+
+        if(storageTodos && storageTodos.length){
+          setTodos(storageTodos.sort((a, b) => b.id - a.id))
+        }else{
+          setTodos(data.sort((a,b) => b.id - a.id))
+        }
+      } catch (e) {
+        console.error(e)
+      }
+    }
+    fetchData()
+  }, [data])
+
+  useEffect(() => {
+    const storeData = async () => {
+      try {
+        // const jsonValue = await AsyncStorage.getItem("TodoApp")
+        const jsonValue = JSON.stringify(todos)
+        await AsyncStorage.getItem("TodoApp", jsonValue)
+      } catch (e) {
+        console.error(e)
+      }
+    }
+    storeData()
+  }, [todos])
+
   if (!loaded && !error) {
     return null
   }
 
-const styles = createStyles(theme, colorScheme)
+  const styles = createStyles(theme, colorScheme)
 
   const addTodo = () => {
     if (text.trim()) {
@@ -73,82 +110,86 @@ const styles = createStyles(theme, colorScheme)
         <Pressable onPress={addTodo} style={styles.addButton}>
           <Text style={styles.addButtonText}>Add</Text>
         </Pressable>
-        <Pressable onPress={()=> setColorScheme(colorScheme === 'light' ? 'dark' : 'light')} style={{marginLeft:10}}>
-          {colorScheme === 'dark' ? <Octicons name="moon" size={36} color={theme.text} selectable={undefined} style={{width: 36}} />
-        :  <Octicons name="sun" size={36} color={theme.text} selectable={undefined} style={{width: 36}} />
-        }
+        <Pressable onPress={() => setColorScheme(colorScheme === 'light' ? 'dark' : 'light')} style={{ marginLeft: 10 }}>
+          {colorScheme === 'dark' ? <Octicons name="moon" size={36} color={theme.text} selectable={undefined} style={{ width: 36 }} />
+            : <Octicons name="sun" size={36} color={theme.text} selectable={undefined} style={{ width: 36 }} />
+          }
         </Pressable>
       </View>
-      <FlatList
+      <Animated.FlatList
         data={todos}
         renderItem={renderItem}
         keyExtractor={item => item.id}
         contentContainerStyle={{ flexGrow: 1 }}
+        itemLayoutAnimation={LinearTransition}
+        keyboardDismissMode="on-drag"
       />
+      <StatusBar style={colorScheme === 'dark' ? 'light' : 'dark'} />
     </SafeAreaView>
   );
 }
 
 
-function createStyles(theme, colorScheme){
+function createStyles(theme, colorScheme) {
   return StyleSheet.create({
-  container: {
-    flex: 1,
-    // width: '100%',
-    backgroundColor: theme.background,
-  },
-  inputContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    marginBottom: 10,
-    padding: 10,
-    width: '100%',
-    maxWidth: 1024,
-    marginHorizontal: 'auto',
-    pointerEvents: 'auto',
-  },
-  input: {
-    flex: 1,
-    borderColor: 'gray',
-    borderWidth: 1,
-    borderRadius: 5,
-    padding: 10,
-    marginRight: 10,
-    fontSize: 18,
-    fontFamily: 'Inter_500Medium',
-    minWidth: 0,
-    color: theme.text,
-  },
-  addButton: {
-    backgroundColor: theme.button,
-    borderRadius: 5,
-    padding: 10,
-  },
-  addButtonText: {
-    fontSize: 18,
-    color: colorScheme === 'dark' ? 'black' : 'white',
-  },
-  todoItem: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'space-between',
-    gap: 4,
-    padding: 10,
-    borderBottomColor: 'gray',
-    borderBottomWidth: 1,
-    width: '100%',
-    maxWidth: 1024,
-    marginHorizontal: 'auto',
-    pointerEvents: 'auto',
-  },
-  todoText: {
-    flex: 1,
-    fontSize: 18,
-    fontFamily: 'Inter_500Medium',
-    color: theme.text,
-  },
-  completedText: {
-    textDecorationLine: 'line-through',
-    color: 'gray',
-  },
-})};
+    container: {
+      flex: 1,
+      // width: '100%',
+      backgroundColor: theme.background,
+    },
+    inputContainer: {
+      flexDirection: 'row',
+      alignItems: 'center',
+      marginBottom: 10,
+      padding: 10,
+      width: '100%',
+      maxWidth: 1024,
+      marginHorizontal: 'auto',
+      pointerEvents: 'auto',
+    },
+    input: {
+      flex: 1,
+      borderColor: 'gray',
+      borderWidth: 1,
+      borderRadius: 5,
+      padding: 10,
+      marginRight: 10,
+      fontSize: 18,
+      fontFamily: 'Inter_500Medium',
+      minWidth: 0,
+      color: theme.text,
+    },
+    addButton: {
+      backgroundColor: theme.button,
+      borderRadius: 5,
+      padding: 10,
+    },
+    addButtonText: {
+      fontSize: 18,
+      color: colorScheme === 'dark' ? 'black' : 'white',
+    },
+    todoItem: {
+      flexDirection: 'row',
+      alignItems: 'center',
+      justifyContent: 'space-between',
+      gap: 4,
+      padding: 10,
+      borderBottomColor: 'gray',
+      borderBottomWidth: 1,
+      width: '100%',
+      maxWidth: 1024,
+      marginHorizontal: 'auto',
+      pointerEvents: 'auto',
+    },
+    todoText: {
+      flex: 1,
+      fontSize: 18,
+      fontFamily: 'Inter_500Medium',
+      color: theme.text,
+    },
+    completedText: {
+      textDecorationLine: 'line-through',
+      color: 'gray',
+    },
+  })
+};
--- CrudApp/data/todos.js
+++ CrudApp/data/todos.js
@@ -23,5 +23,55 @@ export const data = [
         "id": 5,
         "title": "Finish the project",
         "completed": true,
+    },
+    {
+        "id": 6,
+        "title": "Finish reading the book",
+        "completed": true,
+    },
+    {
+        "id": 7,
+        "title": "Finish the project",
+        "completed": false,
+    },
+    {
+        "id": 8,
+        "title": "Learn React",
+        "completed": true,
+    },
+    {
+        "id": 9,
+        "title": "Go to the gym",
+        "completed": false,
+    },
+    {
+        "id": 10,
+        "title": "Finish the project",
+        "completed": true,
+    },
+    {
+        "id": 11,
+        "title": "Finish reading the book",
+        "completed": true,
+    },
+    {
+        "id": 12,
+        "title": "Finish the project",
+        "completed": false,
+    },
+    {
+        "id": 13,
+        "title": "Learn React",
+        "completed": true,
+    },
+    {
+        "id": 14,
+        "title": "Go to the gym",
+        "completed": false,
+    },
+    {
+        "id": 15,
+        "title": "Finish the project",
+        "completed": true,
     }
 ]
